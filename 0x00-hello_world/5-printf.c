7a48ad7050182180645150187e2e4cae2e0081ec
Joseph <josephgitau6703@gmail.com>
2022-12-08 07:34:03 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,9 @@
 /**
  * main - Entry Point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
 */
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
