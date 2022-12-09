705364c8c2f8542e2c1cd4e739db9a0e43fbc1d6
Joseph <josephgitau6703@gmail.com>
2022-12-09 04:23:30 -0800

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - assing a random number
+ *
+ * Return: 0 (Success)
+ *
+*/
+int main(void)
+{
+	int n;
+	
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n -- 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0)
