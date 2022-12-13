3a9d031f05beea531ac91b45e6d3346ef3bba6f7
Joseph <josephgitau6703@gmail.com>
2022-12-13 11:11:31 -0800

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ * main - check the code.
+ *
+ * Return: Always 0.
+ *
+*/
+
+int main(void)
+
+{
+	int alpha
+	alpha= 'a';
+
+	while (alpha <= 'z')
+	{
+	    putchar("10 ");
+	    alpha++;
+	}
+	putchar("\n");
+       	return (0);
+
+}
