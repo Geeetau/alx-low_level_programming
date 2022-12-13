adfb0f8dd1eb77e2ad9d600bfca40ff550a2755d
Joseph <josephgitau6703@gmail.com>
2022-12-13 12:06:40 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 /**
- *
  * main - check the code.
  *
  * Return: Always 0.
  *
 */
-
-int print_alphabet_x10(void)
-
+int main(void)
 {
-	int alpha = 'a' , count;
+	int alpha = 'a', count;
 
 	while (alpha <= 'z')
 	{
-		count + 0;
+		count = 0;
 		while (count <= 9)
 	{
 		printf("alpha");
 		count++;
 	}
 	putchar("\n");
-       	return (0);
 	}
+	return (0);
 }
