23628c0ded1de79efa7a3620e137ca7ebd5ceed5
Joseph <josephgitau6703@gmail.com>
2022-12-14 00:51:32 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,6 @@
  *
  * Return: Always 0 (Success)
 */
-int _putchar(char c);
 int main(void)
 {
 	_putchar('_');
@@ -16,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
 	return (0);
 }
