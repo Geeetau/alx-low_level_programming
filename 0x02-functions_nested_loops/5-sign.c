66c5bcc79c2185ae1e782727c883372b07c9500f
Joseph <josephgitau6703@gmail.com>
2022-12-14 03:33:14 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,12 +12,12 @@ int print_sign(int n)
 		return (1);
 		_putchar('+');
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
 		return (0);
 		_putchar('0');
 	}
-	else (n < 0)
+	else
 	{
 		return (-1);
 		_putchar('-');
