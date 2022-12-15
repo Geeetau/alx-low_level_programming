86f450dcd6d4302c70fb3051f828b5d047ade960
Joseph <josephgitau6703@gmail.com>
2022-12-15 14:00:46 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,12 +2,13 @@
 
 /**
  * print_most_numbers - prints most numbers
- * @c: character
  * Return: always 0
  */
 
 void print_most_numbers(void)
 {
+	char c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 	if (!(c == '2' || c == '4'))
