0bb51b1b06e397d1bf6f7353e1c793a879b35e0e
Joseph <josephgitau6703@gmail.com>
2022-12-15 14:27:29 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,12 +6,16 @@
  */
 void more_numbers(void)
 {
-	int j;
+	int h, j;
+	for (h = 1; h <= 10; h++)
+	{
 
 	for (j = '0'; j <= '14'; j++)
 	{
 		if (j >= 10)
+		_putchar('1');
 		_putchar(j % 10 + '0');
 	}
 	_putchar('\n');
+	}
 }
