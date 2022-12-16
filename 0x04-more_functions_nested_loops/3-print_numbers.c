7d074cd59e7b6b1f695edced95e9ee293d78d560
Joseph <josephgitau6703@gmail.com>
2022-12-16 03:10:14 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,15 @@
 
 /**
  * print_numbers - prints num from 0 to 9
- * @c: character
  * Return: void
  */
 void print_numbers(void)
 {
-	int c;
+       char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
