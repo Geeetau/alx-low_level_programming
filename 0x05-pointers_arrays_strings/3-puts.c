e7461d70a7d6ecba201e7143ab1efee5b292e301
Joseph <josephgitau6703@gmail.com>
2022-12-19 12:56:56 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+	int d;
+
+	while (str[d] != '\0')
+	{
+		_putchar(stri[d]);
+		d++;
+	}
+	_putchar('\n');
+}
