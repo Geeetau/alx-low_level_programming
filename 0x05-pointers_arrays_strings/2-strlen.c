848880d0c8b223119e2cb2115eb1e35bbabeb085
Joseph <josephgitau6703@gmail.com>
2022-12-19 12:41:37 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * _strlen - returns the length of a string
+ * @s: char to check
+ * Return: 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+
+	return (a);
+}
