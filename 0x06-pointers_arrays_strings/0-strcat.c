92900dc010f1126e376bb3501e5eda70d32171af
Joseph <josephgitau6703@gmail.com>
2022-12-22 11:43:53 -0800

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: character
+ * @src: characetr
+ * Return: character
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *a = dest;
+
+	while (*dest != '\0')
+		dest++;
+
+	while (*src != '\0')
+
+	*dest = *src;
+	dest++;
+	src;
+
+	dest = '\0';
+	return (a);
+}
