5d0a69993c89a31b84b62cac2893286a101d4419
Joseph <josephgitau6703@gmail.com>
2022-12-22 11:56:48 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,17 @@ char *_strcat(char *dest, char *src)
 	char *a = dest;
 
 	while (*dest != '\0')
+	{
 		dest++;
+	}
 
 	while (*src != '\0')
 
-	*dest = *src;
-	dest++;
-	src;
+	{
+		*dest = *src;
+		dest++;
+		src;
+	}
 
 	dest = '\0';
 	return (a);
