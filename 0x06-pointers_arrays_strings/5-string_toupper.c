81e751a31f38d724f4c8809da33f75ceb6889876
Joseph <josephgitau6703@gmail.com>
2022-12-22 22:29:50 -0800

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,10 +9,10 @@ char *string_toupper(char *s)
 {
 	int a = 0;
 
-	while (s[a] != '\0')
+	while (*(s + a))
 	{
-		if (s[a] >= 'a' && n[a] <= 'z')
-			s[a] = s[a] - 32;
+		if (*(s + a) >= 'a' && *(s + a) <= 'z')
+			*(s + a) -= 'a' - 'A';
 		a++;
 	}
 	return (s);
