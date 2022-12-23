06a7ec8947958774eca4b3a66803fe7faf4a68cf
Joseph <josephgitau6703@gmail.com>
2022-12-22 21:38:21 -0800

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcmp - compares two strings
+ *@s1: input
+ *@s2: input
+ *Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+
+	while (s1[a] != '\0' && s2[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+		a++;
+	}
+	return (0);
+}
