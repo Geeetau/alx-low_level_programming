1c17741503755764a2f028f65537bf2f5979a814
Joseph <josephgitau6703@gmail.com>
2023-01-03 11:42:09 -0800

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strpbrk -  searches a string for any of a set of bytes
+ * @s: first occurrence in the string
+ * @accept: matches one of the bytes or NULL if no such byte
+ * Return: Always 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s = accept[j])
+				return (s);
+		}
+	s++;
+	}
+	return ('\0');
+}
