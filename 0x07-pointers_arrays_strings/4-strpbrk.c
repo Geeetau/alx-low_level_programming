f884cfc8a9d61a42233ed328ca912cb273824f39
Joseph <josephgitau6703@gmail.com>
2023-01-03 11:44:08 -0800

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,11 +9,12 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int j;
+
 	while (*s)
 	{
 		for (j = 0; accept[j]; j++)
 		{
-			if (*s = accept[j])
+			if (*s == accept[j])
 				return (s);
 		}
 	s++;
