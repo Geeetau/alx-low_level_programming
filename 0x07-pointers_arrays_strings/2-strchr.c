2a3374226d9fad79d050da6f57416e5a2fe719e8
Joseph <josephgitau6703@gmail.com>
2023-01-03 11:06:04 -0800

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,13 +4,13 @@
  * _strchr - locates a character in a string
  * @s: string
  * @c: character
- * Return: pointer to the first occurrence of the character
+ * Return: 0
  */
 char *_strchr(char *s, char c)
 {
 	int j = 0;
 
-	for (s[j] >= '\0'; j++)
+	for (; s[j] >= '\0'; j++)
 	{
 		if (s[j] == c)
 			return (&s[j]);
