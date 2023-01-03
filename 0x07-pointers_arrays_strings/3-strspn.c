c86635c0410013fca07236904a52a92748cd8583
Joseph <josephgitau6703@gmail.com>
2023-01-03 11:20:23 -0800

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: Always 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned inj j = 0;
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				j++
+		}
+		else if (accept[r + 1] == '\0')
+			return (j);
+
+		s++;
+	}
+	return (j);
+}
