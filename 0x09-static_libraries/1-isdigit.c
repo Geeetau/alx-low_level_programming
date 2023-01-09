1f345f4ccafda07811e5a966f579fc3cf648c611
Joseph <josephgitau6703@gmail.com>
2023-01-09 14:47:34 -0800

    task 0

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: character
+ * Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: Always 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int j = 0;
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+			{
+				j++;
+				break;
+			}
+		else if (accept[k + 1] == '\0')
+			return (j);
+		}
+		s++;
+	}
+	return (j);
+}
