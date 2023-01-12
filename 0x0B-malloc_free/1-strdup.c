beab5cf89af0366acb558b6b7553c8941de5f6ab
Joseph <josephgitau6703@gmail.com>
2023-01-12 13:32:07 -0800

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,17 +13,19 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
+	j = 0;
 
-	while (str[k] != '\0')
-		k++;
+	while (str[j] != '\0')
+		j++;
 
-	copy = (char *)malloc((sizeof(char) * k) + 1);
-	(copy = NULL);
+	copy = (char *)malloc((sizeof(char) * j) + 1);
+	{
+		 (copy = NULL)
 		return (NULL);
+	}
 
-	for (j = 0; j < k; j++)
-		copy[j] = str[j];
-	copy[k] = '\0';
+	for (k = 0; str[k]; k++)
+		copy[k] = str[k];
 
 	return (copy);
 }
