9c4198925bd67f91ec54c759407c34ea81b0eed0
Joseph <josephgitau6703@gmail.com>
2023-01-12 12:52:05 -0800

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ *@str: string
+ *Return: pointer to the duplicated string, NULL if insufficient memory
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int j, k = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[k] != '\0')
+		k++;
+
+	copy = (char *)malloc(sizeof(char) * k + 1);
+	if (copy == NULL)
+		return (NULL);
+
+	for (j = 0; j < k; j++)
+		copy[j] = str[j];
+	copy[k] = '\0';
+
+	return (copy);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,1 +1,1 @@
-create array
+strduo
