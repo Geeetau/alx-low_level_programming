e005252d990f396731b9a21aea1cc57aa704744c
Joseph <josephgitau6703@gmail.com>
2023-01-13 07:08:11 -0800

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _calloc - allocates memory for an array
  * @size bytes
  * @nmemb: allocate memory for array
+ * @size: allocate element of size bytes
  * Return: pointer to the allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -11,10 +13,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *j;
 	unsigned int k;
 
-	if (nmeb == 0 || size == 0)
+	if (nmeb == 0)
+		return (NULL);
+	if (size == 0)
 		return (NULL);
 	j = malloc(nmeb * size);
-	if (j = NULL)
+	if (j == NULL)
 		return (NULL);
 	for (k = 0; k < (nmeb * size); k++)
 		j[k] = 0;
