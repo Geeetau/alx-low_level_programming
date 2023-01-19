8585c69dd83b4d917658057fd72d9e5932bda1a2
Joseph <josephgitau6703@gmail.com>
2023-01-19 13:55:34 -0800

    task 0

--- 0-print_name.c
+++ 0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - a function that prints a name
@@ -7,6 +8,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name == NULL || f == NULL)
+		return;
+
 		f(name);
 }
