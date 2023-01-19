3227e98f5524fd62a8937de729b6b1e45c34242b
Joseph <josephgitau6703@gmail.com>
2023-01-19 14:08:54 -0800

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given
+ * @array: array to execute function
+ * @size: size of the array
+ * @action: pointer to the function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t j;
+	if (array && action)
+	{
+		j = 0;
+		while (j < size)
+		{
+			action(array[j]);
+			j++;
+		}
+	}
+}
