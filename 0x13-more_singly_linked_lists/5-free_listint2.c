793551fa28f7843c3ee2c9e480e38c904a194df6
Joseph <josephgitau6703@gmail.com>
2023-02-02 15:28:31 -0800

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: a double pointer of lists
+ * Return: Always 0
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -3,3 +3,5 @@ task1 1. Returns number of elements in a list.
 task 2. Adds a new node at the beginning.
 task 3. adds a new noder at the end.
 task 4. Frees a listint list
+task 5. frees a listint list 2
+
