7ee1dc07cb4d8ee29f33a1d17451d07abf343484
Joseph <josephgitau6703@gmail.com>
2023-02-03 04:12:35 -0800

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,17 +11,15 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int j;
-	listint_h *temp, *new;
+	listint_t *temp, *new;
 
 	if (head == NULL)
 		return (NULL);
-	if (idx != 0);
+	if (idx != 0)
 	{
 		temp = *head;
-		for (j = 0; j< idx - 1 && temp != NULL; j++)
-		{
-			temp = temp-> next;
-		}
+		for (j = 0; j < idx -1 && temp != NULL; j++)
+			temp = temp->next;
 		if (temp == NULL)
 			return (NULL);
 	}
