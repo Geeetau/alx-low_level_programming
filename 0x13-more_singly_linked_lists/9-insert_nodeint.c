e66cdddb30a2e63aec0e21c7c79c52e5873a8f2a
Joseph <josephgitau6703@gmail.com>
2023-02-03 04:15:55 -0800

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,7 +5,7 @@
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: double pointer
  * @idx: index of the node
- * @n: new node value 
+ * @n: new node value
  * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx != 0)
 	{
 		temp = *head;
-		for (j = 0; j < idx -1 && temp != NULL; j++)
+		for (j = 0; j < idx - 1 && temp != NULL; j++)
 			temp = temp->next;
 		if (temp == NULL)
 			return (NULL);
