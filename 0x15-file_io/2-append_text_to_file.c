29e0a2bf1c55fd385f1136eb2ae16dbcd1abb733
Joseph <josephgitau6703@gmail.com>
2023-02-08 16:07:26 -0800

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ * Return: length of the string
+ */
+size_t _strlen(char *str)
+{
+	size_t j;
+
+	for (j = 0; str[j]; j++)
+		;
+	return (j);
+}
+
+/**
  * append_text_to_file -  appends text at the end of a file
  * @filename: name of the file
  * @text_content:  is the NULL terminated string to add at the end of the file
