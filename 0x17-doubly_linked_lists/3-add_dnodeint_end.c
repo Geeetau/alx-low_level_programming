357289e02f73dc0df701e7b1caf708a5e2be5d6b
Joseph <josephgitau6703@gmail.com>
2023-03-16 13:56:03 -0700

    task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,14 +20,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new->next = NULL;
 	if (*head == NULL)
 	{
-		new->orev = NULL;
+		new->prev = NULL;
 		*head = new;
 		return (new);
 	}
 	temp = *head;
 	while (tmp->next != NULL)
 		tmp = tmp->next;
-	tmp->next = new;
+	temp->next = new;
 	new->prev = tmp;
 	return (new);
 }
